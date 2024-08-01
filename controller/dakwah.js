b063c9cdae986ec9bc54042b1a2a400504289086
MAHafizh <hafizh12jr@gmail.com>
2024-08-01 15:50:26 +0700

    add katalog images, update op modules in dakwah/katalog

--- controller/dakwah.js
+++ controller/dakwah.js
@@ -4,6 +4,7 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { Op } from "sequelize";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
--- controller/katalog.js
+++ controller/katalog.js
@@ -4,6 +4,7 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { Op } from "sequelize";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -35,6 +36,7 @@ export const getKatalog = async (req, res) => {
     });
     
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: error });
   }
 };
