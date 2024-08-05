da5e269352f4dba5bc39ed6b235c6658fc1fe9ad
Subairi <134286891+subaaaiii@users.noreply.github.com>
2024-08-05 08:18:49 +0700

    remove timezone

--- config/database.js
+++ config/database.js
@@ -8,13 +8,11 @@ const {
   DB_HOSTNAME,
   DB_NAME,
   DB_DIALECT,
-  DB_TIMEZONE,
 } = process.env;
 
 const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOSTNAME,
   dialect: DB_DIALECT,
-  timezone: DB_TIMEZONE,
 });
 
 export default db;
